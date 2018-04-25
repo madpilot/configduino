35b336503368885667ccb8a89b8519f8324985ed
Myles Eftos <myles@madpilot.com.au>
2018-04-25 20:37:59 +1000

    Removing the console log on the firmware panel

--- src/components/firmware/index.js
+++ src/components/firmware/index.js
@@ -5,15 +5,11 @@ import * as Validation from '../../validation/validator.js';
 import styles from './style.css';
 
 export default class FirmwarePanel extends Component {
-  fileSelect(e) {
-    console.log(e.target.value)
-  }
-
   render() {
     return (
       <section className={styles.panel}>
         <h3 className={styles.heading}>Update the firmware</h3>
-        <Input name="file" label="Firmware" type="file" onChange={this.fileSelect.bind(this)} validators={[ Validation.required() ]} />
+        <Input name="file" label="Firmware" type="file" validators={[ Validation.required() ]} />
       </section>
     );
   }
